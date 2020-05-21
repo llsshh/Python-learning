1f0ff40941038b9fea81b64690fbc8e3cbe71842
llsshh <792313704@qq.com>
2020-05-22 00:45:29 +0800

    看看还用不用一直输入密码

--- pyprac.py
+++ pyprac.py
@@ -5,3 +5,4 @@ import torch
 x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
 plt.plot(x, np.sin(x))       # Plot the sine of each x point
 plt.show()                   # Display the plot
+
